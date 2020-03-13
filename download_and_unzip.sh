e3118ac9d85ff9e57580a984e93bc278abc7513b
Leonardo Diegues <leonardo@pop-os.localdomain>
2020-03-13 19:38:43 -0300

    chore: modifica estrutura do repositório e troca arquivos dbf por sav

--- download_and_unzip.sh
+++ download_and_unzip.sh
@@ -5,11 +5,9 @@
 # This script must be executed inside odsp-files folder in order
 # to find its children path folders, so it can do what it must do.
 
-OD_YEARS=`seq 1977 10 2007`  # tr
-
-URL_BASE='http://www.metro.sp.gov.br/pesquisa-od/arquivos/Pesquisa_Origem_Destino-'
-
-DATA_PATH='microdados'
+OD_YEARS=`seq 1977 10 2017`  # tr
+URL_BASE='http://www.metro.sp.gov.br/pesquisa-od/arquivos'
+DATA_PATH='data'
 
 if ! test -d $DATA_PATH; then
     mkdir $DATA_PATH
@@ -17,31 +15,20 @@ fi
 
 for i in $OD_YEARS; do
     echo "Downloading data from: $i survey...";
-    
-    DOWNLOAD_URL="$URL_BASE$i.zip";
-
-    DOWNLOAD_PATH="$DATA_PATH/OD_$i.zip";
-
-    OUTFILE="$DATA_PATH/OD_$i.dbf"
-
-    if test -f "$DOWNLOAD_PATH"; then
-        echo "File already exists: $DOWNLOAD_PATH"
-
+    if [ $i -eq 2017 ]; then
+        DOWNLOAD_URL="$URL_BASE/Pesquisa-Origem-Destino-$i-Banco-Dados.zip"
     else
-        wget $DOWNLOAD_URL -O $DOWNLOAD_PATH;
-        
-        ZIPFILE="$(zipinfo -1 $DOWNLOAD_PATH | egrep "Banco de Dados\\/(.*?)\\.dbf")";
-
-        OUTZIP="$DATA_PATH/$ZIPFILE";
-
-        unzip $DOWNLOAD_PATH "$ZIPFILE" -d "$DATA_PATH";
-
-        mv "$OUTZIP" "$OUTFILE";
-        
-        bzip2 -z $OUTFILE;
-
-        rm -rf "$DATA_PATH/$(ls $DATA_PATH | grep "^Pesquisa")"
-
-        rm $DOWNLOAD_PATH;
+        DOWNLOAD_URL="$URL_BASE/Pesquisa_Origem_Destino-$i.zip"
     fi
+    DOWNLOAD_PATH="$DATA_PATH/OD_$i.zip"
+    OUTFILE="$DATA_PATH/OD_$i.sav"
+    curl $DOWNLOAD_URL -o $DOWNLOAD_PATH
+    ZIPFILE="$(zipinfo -1 $DOWNLOAD_PATH | egrep "\\.sav$")"
+    OUTZIP="$DATA_PATH/$ZIPFILE"
+    unzip $DOWNLOAD_PATH "$ZIPFILE" -d "$DATA_PATH"
+    mv "$OUTZIP" "$OUTFILE"
+    bzip2 -z $OUTFILE
+    cd $DATA_PATH && rm -R -- */
+    cd ../
+    rm $DOWNLOAD_PATH
 done
